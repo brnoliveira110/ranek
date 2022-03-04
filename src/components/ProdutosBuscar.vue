2db90ea1ab2e8807892779eadf32efb9ed2ca25b
Caio Santana <brnoliveirasantana@gmail.com>
2022-03-04 00:21:56 -0300

    best commit

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      busca: "",
+    };
+  },
+
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍Compre ou Venda👎</h1>
+    <ProdutosBusca />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosLista from "@/components/ProdutosLista.vue";
+import ProdutosBusca from "@/components/ProdutosBuscar.vue";
 export default {
   name: "HomeView",
+  components: {
+    ProdutosLista,
+    ProdutosBusca,
+  },
 };
 </script>
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
